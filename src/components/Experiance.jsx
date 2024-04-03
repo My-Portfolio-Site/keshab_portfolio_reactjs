82e6502f66a9ebd1337c627a8962bf10dafd5a82
Keshab Manni <34715906+keshabmanni@users.noreply.github.com>
2024-04-03 19:14:04 +0000

    company wise experiance grouping

--- src/components/Experiance.jsx
+++ src/components/Experiance.jsx
@@ -9,35 +9,41 @@ const Experiance = () => {
         {
             company: "Marlabs Pvt Ltd",
             location: "Banglore, India",
-            position: "Software Engineer",
-            date: "Jan 2024 - Present",
-            details: [
-                "Developed and implemented UiPath automation solutions to streamline business processes, resulting in 50% to 80% increase in process efficiency.",
-                "Collaborated with business stakeholders to gather requirements, design automation workflows and deliver solutions that met their needs.",
-                "Conducted debugging and testing of UiPath workflows to ensure accuracy and reliability.",
-                "Provided ongoing monitoring, support and maintenance for UiPath robots, addressing user inquiries and troubleshooting issues promptly."    
-            ]
-        },
-        {
-            company: "Marlabs Pvt Ltd",
-            location: "Banglore, India",
-            position: "Assocoate Software Engineer",
-            date: "Dec 2021 - Dec 2023",
-            details: [
-                "Provided monitoring and support(L1 & L2) for UiPath automation projects for client(Health & Life Science company). Responded to user inquiries, providing technical assistance and troubleshooting, resolve issues and test. Generate regular reports and updates on system performances.",
-                "Developed python script to process pdf document to extract text information and convert into new pdf while preserving the format.",
-                "Designed and implemented intent based chatbot for Marlabs website to improve visitors interaction and inquiry experiance. Skills: Rasa Chatbot Framework, HTML, CSS, JavaScript, MongoDB (AWS DocumentDB), AWS EC2, Docker and Nginx."
+            positions: [
+                {
+                    designation: "Software Engineer",
+                    date: "Jan 2024 - Present",
+                    details: [
+                        "Developed and implemented UiPath automation solutions to streamline business processes, resulting in 50% to 80% increase in process efficiency.",
+                        "Collaborated with business stakeholders to gather requirements, design automation workflows and deliver solutions that met their needs.",
+                        "Conducted debugging and testing of UiPath workflows to ensure accuracy and reliability.",
+                        "Provided ongoing monitoring, support and maintenance for UiPath robots, addressing user inquiries and troubleshooting issues promptly."
+                    ]
+                },
+                {
+                    designation: "Assocoate Software Engineer",
+                    date: "Dec 2021 - Dec 2023",
+                    details: [
+                        "Provided monitoring and support(L1 & L2) for UiPath automation projects for client(Health & Life Science company). Responded to user inquiries, providing technical assistance and troubleshooting, resolve issues and test. Generate regular reports and updates on system performances.",
+                        "Developed python script to process pdf document to extract text information and convert into new pdf while preserving the format.",
+                        "Designed and implemented intent based chatbot for Marlabs website to improve visitors interaction and inquiry experiance. Skills: Rasa Chatbot Framework, HTML, CSS, JavaScript, MongoDB (AWS DocumentDB), AWS EC2, Docker and Nginx."
+                    ]
+                }
             ]
         },
         {
             company: "Technovert",
             location: "Hydrabad, India",
-            position: "Intern RPA developer",
-            date: "May 2019 - July 2019",
-            details: [
-                "Contributed as part of the automation team to automate employee selection, onboarding, payslip, reimbursement and exit process combining different platforms like naukri.com, KekaHR and excel by using UiPath. With the use of automation able to reduce time and cost significantly.",
-                "Build email automation process to auto send personalized mass emails based on templates using UiPath.",
-                "Learned about automation tools like UiPath, Automation Anywhere."
+            positions: [
+                {
+                    designation: "Intern RPA developer",
+                    date: "May 2019 - July 2019",
+                    details: [
+                        "Contributed as part of the automation team to automate employee selection, onboarding, payslip, reimbursement and exit process combining different platforms like naukri.com, KekaHR and excel by using UiPath. With the use of automation able to reduce time and cost significantly.",
+                        "Build email automation process to auto send personalized mass emails based on templates using UiPath.",
+                        "Learned about automation tools like UiPath, Automation Anywhere."
+                    ]
+                }
             ]
         }
     ]
@@ -45,7 +51,7 @@ const Experiance = () => {
     return (
         <FullScreenSection className="gap-3 bg-gradient-to-b from-orange-100 to-green-100" id="experiance">
             <h2 className="text-[28px] font-extrabold flex flex-row text-purple-500">
-                <img loading="lazy" className='p-1' src={icon_experiance} height="36px" width="36px" alt='experiance'/>
+                <img loading="lazy" className='p-1' src={icon_experiance} height="36px" width="36px" alt='experiance' />
                 Experiance
             </h2>
             {experiances.map(item => <ExperianceComponent key={item.company} {...item} />)}
@@ -53,28 +59,33 @@ const Experiance = () => {
     )
 }
 
-const ExperianceComponent = ({ company, location, position, date, details }) => {
+const ExperianceComponent = ({ company, location, positions }) => {
     return (
         <Reveal className="p-3 bg-slate-100 rounded-lg">
             <div>
                 <span className="text-[20px] p-2 font-semibold text-blue-800">{company},</span>
                 <span className="text-[16px]  font-bold">{location}</span>
             </div>
-            <div className="flex flex-col sm:flex-row p-2">
-                <span className="basis-3/4 text-[14px] md:text-[18px]">{position}</span>
-                <span className="basis-1/4 text-[14px] md:text-[18px]">{date}</span>
-            </div>
-            <div>
-                <ul className="list-disc ml-10">
-                    {details.map((item, index) =>
-                        <li key={index} className="pt-1 text-[15px]">
-                            <Reveal>
-                                {item}
-                            </Reveal>
-                        </li>
-                    )}
-                </ul>
-            </div>
+            {positions.map(pos, index => {
+                <div key={index}>
+                    <div className="flex flex-col sm:flex-row p-2">
+                        <span className="basis-3/4 text-[14px] md:text-[18px]">{pos.designationtion}</span>
+                        <span className="basis-1/4 text-[14px] md:text-[18px]">{pos.date}</span>
+                    </div>
+                    <div>
+                        <ul className="list-disc ml-10">
+                            {pos.details.map((item, index) =>
+                                <li key={index} className="pt-1 text-[15px]">
+                                    <Reveal>
+                                        {item}
+                                    </Reveal>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            })}
+
         </Reveal>
     )
 }
