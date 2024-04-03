8de9fa4ca54a69836de1ff9c0ad5c60d86ca87f1
Keshab Manni <34715906+keshabmanni@users.noreply.github.com>
2024-04-04 02:58:55 +0530

    Insights and Analytics componenet moved inside App

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <App />
-        
-        {/* Vercel functionalities */}
-        <Analytics />
-        <SpeedInsights />
-    </>
+    <App />
 )
