3d1bb7d9f8f530ea1a8b835d25ef524d7907f09f
Keshab Manni <keshabmanni22@gmail.com>
2025-06-03 22:10:42 +0545

    Few more changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,20 @@
 /* eslint-disable react/prop-types */
 
 import { Reveal } from './Reveal'
-import { motion as m } from 'framer-motion'
 
 const Card = ({ children, ...props }) => {
   return (
-    <Reveal direction='down'>
-      <m.div
-        whileHover={{ scale: 1.02 }}
-        whileTap={{ scale: 0.98 }}
-        transition={{ type: "spring", stiffness: 400, damping: 17 }}
-        className='p-3 bg-surface rounded-xl shadow-lg'
-        {...props}
-      >
-        {children}
-    </m.div>
+    <Reveal
+      direction='down'
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.98 }}
+      transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+      className='p-3 bg-surface rounded-xl shadow-lg'
+      {...props}
+    >
+      {children}
     </Reveal>
   )
 }
 
-export default Card;
+export default Card
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,28 +1,37 @@
 import icon_education from '../assets/icons/education.svg'
 
-import PageSection from "./PageSection";
-import { Reveal } from "./Reveal";
-import Card from './Card';
+import PageSection from './PageSection'
+import { Reveal } from './Reveal'
+import Card from './Card'
 
 const Education = () => {
-    return (
-        <PageSection title={"Education"} icon_src={icon_education} id="education">
-            <Card className="w-full">
-                <div className='px-5 py-5 bg-surface rounded-lg shadow-lg'>
-
-                <Reveal className="flex flex-col sm:flex-row gap-2 sm:gap-10">
-                    <span className="font-bold text-[14px] sm:text-[18px]">B.Tech, Computer Science and Engineering</span>
-                    <span className="font-semibold text-sm sm:text-base">Sep 2016 - July 2021</span>
-                </Reveal>
-                <Reveal className="flex flex-col sm:flex-row gap-1 text-sm sm:text-base">
-                    <span className="font-semibold">Aditya Engineering College, JNTUK</span>
-                    <span className="">(Surampalem, Andhra Pradesh, India)</span>
-                </Reveal>
-                </div>
-            </Card>
-        </PageSection>
-    )
+  return (
+    <PageSection title={'Education'} icon_src={icon_education} id='education'>
+      <Card className='card p-5 md:p-8 bg-surface w-full max-w-2xl'>
+        <Reveal className='flex flex-col sm:flex-row gap-2 sm:gap-10 items-stretch'>
+          <span className='font-bold text-[18px] sm:text-[18px]'>B.Tech</span>
+        </Reveal>
+        <Reveal>
+          <span className='font-semibold text-xm sm:text-base'>
+            Sep 2016 - July 2021
+          </span>
+        </Reveal>
+        <Reveal className='mt-2'>
+          <span className='font-bold text-[14px] sm:text-[18px] text-primary mt-2'>
+            Computer Science and Engineering
+          </span>
+        </Reveal>
+        <Reveal className='flex flex-col sm:flex-row gap-1 text-sm sm:text-base'>
+          <span className='font-semibold'>
+            Aditya Engineering College, JNTUK
+          </span>
+        </Reveal>
+        <Reveal className='flex flex-col sm:flex-row gap-1 text-sm sm:text-base'>
+          <span className=''>(Surampalem, Andhra Pradesh, India)</span>
+        </Reveal>
+      </Card>
+    </PageSection>
+  )
 }
-
 
 export default Education
